33c909a8778cf3c2d0a8d22f71aaa13a0ce45361
Pigrabbit <donghyuk1003+github@gmail.com>
2020-10-21 20:34:46 +0900

    chore: remove unused code in entry App.js

--- App.tsx
+++ App.tsx
@@ -10,17 +10,6 @@
 
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native'
-
-import { Colors } from 'react-native/Libraries/NewAppScreen'
-import styled from 'styled-components/native'
 import HomeScreenStack from './src/routes'
 
 declare const global: { HermesInternal: null | {} }
@@ -28,44 +17,9 @@ declare const global: { HermesInternal: null | {} }
 const App = () => {
   return (
     <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <Section>
-            <HomeScreenStack />
-          </Section>
-        </ScrollView>
-      </SafeAreaView>
+      <HomeScreenStack />
     </NavigationContainer>
   )
 }
-
-const Section = styled.View`
-  background-color: #fff;
-  width: 100%;
-`
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-})
 
 export default App
