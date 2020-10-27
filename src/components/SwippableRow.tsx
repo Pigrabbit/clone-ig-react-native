ba56c865c5f93b3924d33c36f0ef9f55d1f06f77
Pigrabbit <donghyuk1003+github@gmail.com>
2020-10-27 21:08:20 +0900

    styles: SwippableRow 컴포넌트 스타일 구현

--- src/components/SwippableRow.tsx
+++ src/components/SwippableRow.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, Dimensions } from 'react-native';
+import { Dimensions, Text } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
+import styled from 'styled-components/native';
 
 const deviceWidth = Dimensions.get('window').width;
 
@@ -10,17 +11,53 @@ const SwippableRow: React.FC = () => {
       horizontal={true}
       pagingEnabled={true}
       showsHorizontalScrollIndicator={false}>
-      <View style={{ width: deviceWidth }}>
+      <MainSlider style={{ width: deviceWidth }}>
         <Text>Swipe me!</Text>
-      </View>
-      <View style={{ width: deviceWidth }}>
-        <Text>Swipe me 22</Text>
-      </View>
-      <View style={{ width: deviceWidth }}>
-        <Text>Swipe me 333</Text>
-      </View>
+      </MainSlider>
+      <ControlSlider>
+        <ControlSliderEdit>
+          <Text>Edit</Text>
+        </ControlSliderEdit>
+        <ControlSliderDelete>
+          <Text>Delete</Text>
+        </ControlSliderDelete>
+      </ControlSlider>
     </ScrollView>
   );
 };
+
+const MainSlider = styled.View`
+  width: ${deviceWidth}px;
+  height: 36px;
+  justify-content: center;
+  background-color: #ababab;
+`;
+
+const ControlSlider = styled.View`
+  width: ${deviceWidth / 3}px;
+  height: 36px;
+  flex-direction: row;
+`;
+
+const colors = {
+  red: '#eb4d4b',
+  yellow: '#f9ca24',
+};
+
+const ControlSliderEdit = styled.View`
+  width: 50%;
+  padding: 0 5px;
+  background-color: ${colors.yellow};
+  justify-content: center;
+  align-items: center;
+`;
+
+const ControlSliderDelete = styled.View`
+  width: 50%;
+  padding: 0 5px;
+  background-color: ${colors.red};
+  justify-content: center;
+  align-items: center;
+`;
 
 export default SwippableRow;
