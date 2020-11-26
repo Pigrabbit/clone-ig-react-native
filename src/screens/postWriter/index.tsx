a639caa8affe53a5725003e77d1026a4fe2ab4fc
Pigrabbit <donghyuk1003+github@gmail.com>
2020-11-26 23:41:32 +0900

    feat: photo library에서 이미지 불러오는 기능 구현

--- src/screens/postWriter/index.tsx
+++ src/screens/postWriter/index.tsx
@@ -2,11 +2,15 @@ import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 import TouchableIcon from 'components/TouchableIcon';
 import { COLOR } from 'constants/styles';
 import { RootTabParamList } from 'navigators';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from 'stores';
 import { AppTheme } from 'stores/theme/types';
 import styled from 'styled-components/native';
+import ImagePicker, { ImagePickerOptions } from 'react-native-image-picker';
+import { Alert, Image } from 'react-native';
+import { useState } from 'react';
+import { Maybe } from 'components/Maybe';
 
 interface Props {
   navigation: BottomTabNavigationProp<RootTabParamList, 'PostWriter'>;
@@ -16,6 +20,36 @@ const PostWriterScreen: React.FC<Props> = ({ navigation }) => {
   const theme = useSelector<RootState, AppTheme>(
     (rootState) => rootState.theme,
   );
+  const [imageMetaData, setImageMetaData] = useState({
+    uri: '',
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    chooseImageFile();
+  }, []);
+
+  const chooseImageFile = () => {
+    const options: ImagePickerOptions = {
+      title: 'Select Image to post',
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.showImagePicker(options, (response) => {
+      if (response.didCancel) {
+        navigation.goBack();
+      } else if (response.error) {
+        console.error(`ImagePickerError: ${response.error}`);
+      } else {
+        const { uri, width, height } = response;
+        setImageMetaData({ uri, width, height });
+      }
+    });
+  };
+
   return (
     <Container>
       <Header>
@@ -31,6 +65,18 @@ const PostWriterScreen: React.FC<Props> = ({ navigation }) => {
           <HeaderNextButtonText>Next</HeaderNextButtonText>
         </HeaderNextButton>
       </Header>
+      <Body>
+        <Maybe isVisible={imageMetaData.uri !== ''}>
+          <Image
+            source={{ uri: imageMetaData.uri }}
+            style={{
+              width: '100%',
+              height: 'auto',
+              aspectRatio: imageMetaData.width / imageMetaData.height,
+            }}
+          />
+        </Maybe>
+      </Body>
     </Container>
   );
 };
@@ -59,6 +105,11 @@ const HeaderNextButton = styled.TouchableOpacity`
 const HeaderNextButtonText = styled.Text`
   font-size: 17px;
   color: #00a8ff;
+`;
+
+const Body = styled.View`
+  align-items: center;
+  padding: 0 16px;
 `;
 
 export default PostWriterScreen;
