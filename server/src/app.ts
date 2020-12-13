d81837311c954d83f5dc36337b5c8fd032fe1299
Pigrabbit <donghyuk1003+github@gmail.com>
2020-12-13 23:10:50 +0900

    config: server prisma schema 추가

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,11 @@ import express, { Application, Request, Response } from "express";
 const app: Application = express();
 const PORT = 3000;
 
+app.get('/post/:id', (req, res) => {
+  const { id } = req.params
+  
+})
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("Hello TS express!");
 });
